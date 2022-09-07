6b27b2cb5802f6e05b406aef26ec006ab5410f91
CarolinaCedro <yotechgirl@gmail.com>
2022-09-06 23:16:32 -0300

    feat: finish entities on project

---
+++ src/main/java/io/github/CarolinaCedro/HotelManager/domain/entities/HouseKeeping.java
@@ -0,0 +1,19 @@
+package io.github.CarolinaCedro.HotelManager.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HouseKeeping {
+    private String Name;
+    private Integer Id;
+    private String Location;
+}
