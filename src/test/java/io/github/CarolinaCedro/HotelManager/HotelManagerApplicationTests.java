3f082ae16a36368d64f4929401d44d3b260a2a59
CarolinaCedro <yotechgirl@gmail.com>
2022-09-06 22:53:35 -0300

    feat: init project

---
+++ src/test/java/io/github/CarolinaCedro/HotelManager/HotelManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.CarolinaCedro.HotelManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
